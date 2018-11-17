7b793950f7f659b9ba44dff3d19f356d7f036c4a
Joseph Walsh <lighthouse@localhost.localdomain>
2018-11-17 00:57:57 -0500

    Reorganized methods

--- login.py
+++ login.py
@@ -1,44 +1,36 @@
-def main():
-    from selenium import webdriver
-    from selenium.webdriver.common.keys import Keys
-    from selenium.webdriver.chrome.options import Options
-    from selenium.webdriver.common.action_chains import ActionChains
-    import datetime
-    import time
-    import getpass
+import getpass
+from selenium import webdriver
+import datetime
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+class Semester_Registration:    
+    #from selenium.webdriver.common.keys import Keys
+    #from selenium.webdriver.chrome.options import Options
+    #from selenium.webdriver.common.action_chains import ActionChains
+    #import datetime
+    #import time
 
     chrome_executable_path = "/usr/bin/chromedriver"
-    url = 'https://login.rowan.edu/cas/login?TARGET=https%3A%2F%2Fbanner9.rowan.edu%2FStudentRegistrationSsb%2Fj_spring_cas_security_check'
-    get_login_info()
+    
 
-    CRNs = [] #Array of Course Registration Numbers
-    #print("Enter the Course Registration Numbers, and -1 when finished")
-    CRN = 0 #Initialize at arbitrary value
-    #while True:
-    #   CRN = int(input("Class Number: "))
-    #   if CRN == -1: 
-    #       break
-    #   else:
-    #       CRNs.append(CRN)
-    #for CRN in CRNs:
-    #   print(CRN)
-
+    def get_classes(self):
+        CRNs = [] #Array of Course Registration Numbers
+        print("Enter the Course Registration Numbers, and -1 when finished")
+        CRN = 0 #Initialize at arbitrary value
+        while True:
+           CRN = int(input("Class Number: "))
+           if CRN == -1: 
+               break
+           else:
+               CRNs.append(CRN)
+        for CRN in CRNs:
+           print(CRN)
+ 
     #while datetime.datetime.now() < target_time: #waits for desired time
     #   time.sleep(0.25)
-    options=webdriver.ChromeOptions()
-    options.add_argument('--no-sandbox')
-    options.binary_location = '/usr/bin/google-chrome'
-    print("Opening Browser...")
-    browser = webdriver.Chrome(executable_path='/usr/bin/chromedriver', chrome_options=options)
-    browser.get(url)
-    print("Logging in...")
-    browser.find_element_by_name('username').send_keys(username)
-    browser.find_element_by_name('password').send_keys(password, Keys.RETURN)
+    
 
-https://github.com/walshj9/Class_Registration.git
-
-
-    def get_password():
+    def get_password(self):
         while True:
             password1 = getpass.getpass("Password: ")
             password2 = getpass.getpass("Please re-enter the password: ")
@@ -49,16 +41,23 @@ https://github.com/walshj9/Class_Registration.git
     	        print("Passwords didn't match, please try again")
         return password1
 
-    def get_login_info(a, b):
+    def get_login_date(self):
         print("What's the month and date of registration?")
         month = int(input("Month (1-12): "))
         date = int(input("Date: "))
-        target_time = datetime.datetime(today().year, month, date, 7)
+
+    def get_username(self):
         username = input('Enter your username: ')
-        password1 = getpass.getpass('Password: ')
-        password2 = getpass.getpass('Please confirm password: ')
-        if password1 == password2:
-    	    pass
-        else:
-            print("Passwords didn't match, please re-enter login information.")
-            get_password()
+        return username
+
+    def login(self, username, password):
+        login_url = 'https://login.rowan.edu/cas/login?TARGET=https%3A%2F%2Fbanner9.rowan.edu%2FStudentRegistrationSsb%2Fj_spring_cas_security_check'
+        options=webdriver.ChromeOptions()
+        options.add_argument('--no-sandbox')
+        options.binary_location = '/usr/bin/google-chrome'
+        print("Opening Browser...")
+        browser = webdriver.Chrome(executable_path='/usr/bin/chromedriver', chrome_options=options)
+        browser.get(login_url)
+        print("Logging in...")
+        browser.find_element_by_name('username').send_keys(username)
+        browser.find_element_by_name('password').send_keys(password, Keys.RETURN)
--- login_main.py
+++ login_main.py
@@ -1,3 +1,9 @@
-import login
+from login import *
 
-print("Worked")
+if __name__ in '__main__':
+    student = Semester_Registration()
+    student.get_classes()
+    username = student.get_username()
+    password = student.get_password()
+    student.login(username, password)
+    input("Press enter when ready.")
